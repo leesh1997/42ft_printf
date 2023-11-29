8c064f1a229a1bc9645f3dd67745e6dafa396795
이승훈 <95062919+leesh1997@users.noreply.github.com>
2023-11-29 20:21:21 +0900

    ft_printf
    ft_printf

--- ft_printf_str.c
+++ ft_printf_str.c
@@ -6,7 +6,7 @@
 /*   By: seunghun <seunghun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 10:05:52 by seunghun          #+#    #+#             */
-/*   Updated: 2023/11/28 16:31:19 by seunghun         ###   ########.fr       */
+/*   Updated: 2023/11/29 16:06:03 by seunghun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ int	printf_str(char *s)
 	}
 	else
 	{
-		while (s[cnt])
-			cnt++;
 		if ((ft_putstr_fd(s, 1)) == -1)
 			return (-1);
+		while (s[cnt])
+			cnt++;
 		return (cnt);
 	}
 }
